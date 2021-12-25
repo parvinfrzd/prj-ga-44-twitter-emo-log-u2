d4dd9cacb13cc86da9f253410d1514519f88fdda
Parvin Farahzadeh <parv93farah@gmail.com>
2021-12-24 20:54:48 -0500

    finish google oauth

--- twitter-server/routes/users.js
+++ twitter-server/routes/users.js
@@ -2,6 +2,6 @@ var express = require('express');
 var router = express.Router();
 var userCtrl = require('../controllers/users');
 /* GET users listing. */
-router.get('/', userCtrl.index);
+router.get('/users', userCtrl.index);
 
 module.exports = router;
